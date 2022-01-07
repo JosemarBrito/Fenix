66ba74cea5e7f80591b7a8542d4ca3027376a0a4
josemar <estudos.josemarbrito@gmail.com>
2022-01-07 00:24:52 -0300

    Configurado base de testes com uso de fixture e conftest. Ambiente isolado para testes

---
+++ libpythonpro/tests/test_usuarios.py
@@ -0,0 +1,14 @@
+from libpythonpro.spam.modelos import Usuario
+
+
+def test_salvar_usuario(sessao):
+    usuario = Usuario(nome='Josemar')
+    sessao.salvar(usuario)
+    assert isinstance(usuario.id, int)
+
+
+def test_listar_usuarios(conexao, sessao):
+    usuarios = [Usuario(nome='Josemar'), Usuario(nome='Guilherme')]
+    for usuario in usuarios:
+        sessao.salvar(usuario)
+    assert usuarios == sessao.listar()
