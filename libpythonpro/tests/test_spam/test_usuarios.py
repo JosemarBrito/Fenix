9ff14f85ed3d0e08a0ea26f51e1b1b4d9f119dd8
josemar <estudos.josemarbrito@gmail.com>
2022-01-10 22:43:27 -0300

    Configurado base de testes com injeção de dependencias e Mock
    close #Base_de_testes

--- libpythonpro/tests/test_spam/test_usuarios.py
+++ libpythonpro/tests/test_spam/test_usuarios.py
@@ -2,13 +2,15 @@ from libpythonpro.spam.modelos import Usuario
 
 
 def test_salvar_usuario(sessao):
-    usuario = Usuario(nome='Josemar')
+    usuario = Usuario(nome='Josemar', email='estudos.josemarbrito@gmail.com')
     sessao.salvar(usuario)
     assert isinstance(usuario.id, int)
 
 
 def test_listar_usuarios(conexao, sessao):
-    usuarios = [Usuario(nome='Josemar'), Usuario(nome='Guilherme')]
+    usuarios = [Usuario(nome='Josemar', email='estudos.josemarbrito@gmail.com'),
+                Usuario(nome='Guilherme', email='josemarbritosantos@gmail.com')
+                ]
     for usuario in usuarios:
         sessao.salvar(usuario)
     assert usuarios == sessao.listar()
