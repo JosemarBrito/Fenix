a743268687d34767379edb4255ee1c8d97f5f161
josemar <estudos.josemarbrito@gmail.com>
2022-01-10 22:43:27 -0300

    Configurado base de testes com injeção de dependencias e Mock
    close #Base_de_testes

---
+++ libpythonpro/tests/test_spam/test_envio_para_base_de_usuarios.py
@@ -0,0 +1,49 @@
+from unittest.mock import Mock
+
+import pytest
+
+from libpythonpro.spam.main import EnviadorDeSpam
+from libpythonpro.spam.modelos import Usuario
+
+
+@pytest.mark.parametrize(
+    ' usuarios',
+    [
+        [
+            Usuario(nome='Josemar', email='estudos.josemarbrito@gmail.com'),
+            Usuario(nome='Guilherme', email='josemarbritosantos@gmail.com')
+        ],
+        [
+            Usuario(nome='Josemar', email='estudos.josemarbrito@gmail.com'),
+        ]
+    ]
+)
+def test_qtd_de_spam(sessao, usuarios):
+    for usuario in usuarios:
+        sessao. salvar(usuario)
+    enviador = Mock()
+    enviador_de_spam = EnviadorDeSpam(sessao, enviador)
+    enviador_de_spam.enviar_emails(
+        'estudos.josemarbrito@gmail.com',
+        'Curso de Python BD',
+        'Os melhores curso do mercado'
+    )
+    assert len(usuarios) == enviador.enviar.call_count
+
+
+def test_parametros_de_spam(sessao):
+    usuario = Usuario(nome='Josemar', email='estudos.josemarbrito@gmail.com')
+    sessao.salvar(usuario)
+    enviador = Mock()
+    enviador_de_spam = EnviadorDeSpam(sessao, enviador)
+    enviador_de_spam.enviar_emails(
+        'josemarbritosantos@gmail.com',
+        'Curso de Python BD',
+        'Os melhores curso do mercado'
+    )
+    enviador.enviar.assert_called_once_with(
+        'josemarbritosantos@gmail.com',
+        'estudos.josemarbrito@gmail.com',
+        'Curso de Python BD',
+        'Os melhores curso do mercado'
+    )
